cd55fa5aa69b0c14a5116f3a67083f27f07d1a56
gabrielfelps <gabrielfelps.dev@gmail.com>
2026-01-11 13:28:25 -0300

    Adiciona o Capítulo: React Hooks
    Junto da pasta raiz do capítulo, também é adicionado o material inicial da primeira aula: useState.
    Nesse material inicial é abordado sobre o Estado dos componentes, é explicado um pouco mais sobre o que são Hooks em React.
    É tambem discutido sobre a função useState e seu modo de operar, os dados aceitos por ela e como é o processo de trabalhar com Props (propriedades) + Hooks

---
+++ 2. React Hooks/1. useState/src/Modal.jsx
@@ -0,0 +1,11 @@
+const Modal = ({ modal, setModal }) => {
+  if (modal)
+    return (
+      <div>
+        Esse é um modal. <button onClick={() => setModal(false)}>Fechar</button>
+      </div>
+    );
+  return null;
+};
+
+export default Modal;
