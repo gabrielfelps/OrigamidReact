4fdf6d5862319f6891f0f01735008c1c7f183dff
gabrielfelps <gabrielfelps.dev@gmail.com>
2026-01-13 14:26:08 -0300

    Adiciona o Conteúdo: useRef
    Nesta aula foi abordar sobre o useRef e como ele funciona.
    O React precisa ter controle de todo o DOM, por isso não será usado mais querySelector, e sim useRef para selecionar e conectar um elemento do DOM com a nossa lógica dentro de componentes e estados.
    Sem falar que também podemos guardar valor dentro dele, e ao contrario do useState, o useRef não solicita ao React para renderizar a tela novamente.

---
+++ 2. React Hooks/3. useRef/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState, useRef, useEffect } from 'react';
+
+function App() {
+  const [carrinho, setCarrinho] = useState(0);
+  const [notificacao, setNotificacao] = useState(null);
+  const timeoutRef = useRef();
+
+  function handleClick() {
+    setCarrinho((carrinho) => carrinho + 1);
+    setNotificacao('Item adicionado ao carrinho.');
+
+    clearTimeout(timeoutRef.current);
+    timeoutRef.current = setTimeout(() => setNotificacao(null), 2000);
+  }
+
+  return (
+    <div>
+      <p>{notificacao}</p>
+      <button onClick={handleClick}>Adicionar ao Carrinho {carrinho}</button>
+    </div>
+  );
+}
+
+export default App;
