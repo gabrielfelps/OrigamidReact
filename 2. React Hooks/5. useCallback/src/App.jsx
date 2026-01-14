b0c318a35cdb1c5f1689183f97aa63a35d0aa736
gabrielfelps <gabrielfelps.dev@gmail.com>
2026-01-14 13:29:36 -0300

    Adiciona o Conteúdo: useCallback
    Nessa aula foi discutido sobre o useCallback, é um hook que tal como o useMemo irá "memorizar" algo, dessa vez, a função inteira e não só o seu resultado. Seu objetivo é criar uam função e não recriala até algo dentro dela mudar.
    Como crair funções é algo rápido e barato, em pouquissimos momentos utilizaremos a useCallback.

---
+++ 2. React Hooks/5. useCallback/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  const [contar, setContar] = React.useState(0);
+
+  const handleClick = React.useCallback(() => {
+    setContar((contar) => contar + 1);
+  }, []);
+
+  return <button onClick={handleClick}>{contar}</button>;
+}
+
+export default App;
