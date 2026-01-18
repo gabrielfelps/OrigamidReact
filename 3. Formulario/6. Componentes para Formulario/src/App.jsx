eaec78664dd6195579900e756910d52dafc4be13
gabrielfelps <gabrielfelps.dev@gmail.com>
2026-01-18 14:51:59 -0300

    Adiciona o Conteúdo: Componentização do Select
    Nessa aula é cobrado nosso conhecimento da aula de Select. Dessa vez usando o que aprendemos para componentiza-lo e torna-lo utilizavel através da nossa aplicação.

--- 3. Formulario/6. Componentes para Formulario/src/App.jsx
+++ 3. Formulario/6. Componentes para Formulario/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import Input from './Form/Input';
+import Select from './Form/Select';
 
 function App() {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
+  const [select, setSelect] = useState('');
 
   return (
     <form>
@@ -15,6 +17,11 @@ function App() {
         required
       />
       <Input id="email" label={'Email'} value={email} setValue={setEmail} />
+      <Select
+        options={['laptop', 'smartphone', 'tablet']}
+        value={select}
+        setValue={setSelect}
+      />
     </form>
   );
 }
