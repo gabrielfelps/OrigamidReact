73d3c1bfbca6ca8884cac1a8b1c62f6e8a2b5e5e
gabrielfelps <gabrielfelps.dev@gmail.com>
2026-01-18 15:55:54 -0300

    Adiciona o Conteúdo: Componentização do Checkbox
    Nessa aula é cobrado nosso conhecimento das aulas de Checkbox. Dessa vez usando o que aprendemos para componentiza-lo e torna-lo utilizavel através da nossa aplicação.

--- 3. Formulario/6. Componentes para Formulario/src/App.jsx
+++ 3. Formulario/6. Componentes para Formulario/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Input from './Form/Input';
 import Select from './Form/Select';
 import Radio from './Form/Radio';
+import Checkbox from './Form/Checkbox';
 
 function App() {
   const [nome, setNome] = useState('');
@@ -9,9 +10,12 @@ function App() {
   const [select, setSelect] = useState('');
   const [cor, setCor] = useState('');
   const [frutas, setFrutas] = useState('');
+  const [linguagens, setLinguagens] = useState([]);
+  const [termos, setTermos] = useState([]);
 
   return (
     <form>
+      <h2>Dados</h2>
       <Input
         id="nome"
         label={'Nome'}
@@ -20,15 +24,39 @@ function App() {
         required
       />
       <Input id="email" label={'Email'} value={email} setValue={setEmail} />
+      <h3>Produto</h3>
       <Select
         options={['laptop', 'smartphone', 'tablet']}
         value={select}
         setValue={setSelect}
       />
+
       <h2>Cores</h2>
       <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
       <h2>Frutas</h2>
       <Radio options={['Banana', 'Maça']} value={frutas} setValue={setFrutas} />
+
+      <h2>Linguagens</h2>
+      <h3>Frontend 🎨</h3>
+      <Checkbox
+        options={['Javascript', 'Typescript']}
+        value={linguagens}
+        setValue={setLinguagens}
+      />
+
+      <h3>Backend 👨‍💻</h3>
+      <Checkbox
+        options={['Java', 'Go Lang', 'Node']}
+        value={linguagens}
+        setValue={setLinguagens}
+      />
+
+      <h4>Termos e Condições</h4>
+      <Checkbox
+        options={['Li e aceito os termos.']}
+        value={termos}
+        setValue={setTermos}
+      />
     </form>
   );
 }
