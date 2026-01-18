56677e07eaf009fa320b65f74d540486151c6df6
gabrielfelps <gabrielfelps.dev@gmail.com>
2026-01-18 15:11:17 -0300

    Adiciona o Conteúdo: Componentização do Radio
    Nessa aula é cobrado nosso conhecimento da aula de Radio. Dessa vez usando o que aprendemos para componentiza-lo e torna-lo utilizavel através da nossa aplicação.

--- 3. Formulario/6. Componentes para Formulario/src/App.jsx
+++ 3. Formulario/6. Componentes para Formulario/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import Input from './Form/Input';
 import Select from './Form/Select';
+import Radio from './Form/Radio';
 
 function App() {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [select, setSelect] = useState('');
+  const [cor, setCor] = useState('');
+  const [frutas, setFrutas] = useState('');
 
   return (
     <form>
@@ -22,6 +25,10 @@ function App() {
         value={select}
         setValue={setSelect}
       />
+      <h2>Cores</h2>
+      <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
+      <h2>Frutas</h2>
+      <Radio options={['Banana', 'Maça']} value={frutas} setValue={setFrutas} />
     </form>
   );
 }
