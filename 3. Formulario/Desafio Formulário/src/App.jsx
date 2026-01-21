fe977299a42ce911db94156b161b8f8fa6d09a6b
gabrielfelps <gabrielfelps.dev@gmail.com>
2026-01-21 13:59:20 -0300

    Adiciona o Desafio: Formulários
    Desafio proposto pelo professor para testar nossos conhecimento até aqui no curso.

---
+++ 3. Formulario/Desafio Formulário/src/App.jsx
@@ -0,0 +1,11 @@
+import Formulario from './Formulario';
+
+function App() {
+  return (
+    <section>
+      <Formulario />
+    </section>
+  );
+}
+
+export default App;
---
+++ 3. Formulario/Desafio Formulário/src/Botao.jsx
@@ -0,0 +1,9 @@
+const Botao = ({ disabled }) => {
+  return (
+    <button disabled={disabled} style={{ marginTop: '12px' }}>
+      Próxima
+    </button>
+  );
+};
+
+export default Botao;
